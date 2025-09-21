6f286910094bc23cbf3d119e639335a9894fbf8c
son-1u <daniel.syhatheb@yahoo.com>
2025-09-20 21:01:04 -0600

    Update and rename Camera.lua to camera.lua

--- Vehicle/Scripts/camera.lua
+++ Vehicle/Scripts/camera.lua
@@ -1,10 +1,18 @@
+--[[
+Simple camera controller, I might allow for extra features (darkening, shaking, speed-up effect)
+]]--
+
+-------------------------------SERVICES-------------------------------
+
 local RunService = game:GetService("RunService")
+local ContextActionService = game:GetService("ContextActionService")
+
+-----------------------------CAMERA CLASS-----------------------------
 
 local Camera = {}
 Camera.__index = Camera
 
 export type Camera = typeof(setmetatable({} :: {
-	_vehicle: Vehicle,
 	enabled: boolean,
 	_camera: Instance, -- typeof(workspacce.CurrentCamera) returned Instance
 	_current_camera_position: Attachment,
@@ -15,21 +23,20 @@ export type Camera = typeof(setmetatable({} :: {
 	update: (self: Camera) -> (),
 	enable: (self: Camera) -> (),
 	disable: (self: Camera) -> (),
+	destroy: (self: Camera) -> (),
 }, Camera))
 
-function Camera.new(vehicle: Vehicle, camera_positions: {Attachment}): Camera
-	local self = setmetatable({}, Camera)
-	self._vehicle = vehicle
-	self.enabled = true
-	self._camera = workspace.CurrentCamera
-	self._current_camera_position = nil
-	self._camera_positions = camera_positions
-	
-	return self
+function Camera.new(camera_positions: {Attachment}): Camera
+	return setmetatable({
+		enabled = true,
+		_camera = workspace.CurrentCamera,
+		_current_camera_position = nil,
+		_camera_positions = camera_positions
+	}, Camera)
 end
 
 local index = 1
-function Camera.change_camera(self: Camera): ()
+function Camera.change_camera(self: Camera): () -- Would use a linked list, but oh well
 	index += 1
 	if index > #self._camera_positions then
 		index = 1
@@ -47,6 +54,8 @@ function Camera.enable(self: Camera): ()
 	end
 	self.enabled = true
 	
+	ContextActionService:BindAction("CHANGE_CURRENT_CAMERA", self:change_camera(), false, Enum.KeyCode.V)
+	
 	RunService:BindToRenderStep("UPDATE_CAMERA", Enum.RenderPriority.Camera, function()
 		self:update()
 	end)
@@ -58,7 +67,18 @@ function Camera.disable(self: Camera)
 	end
 	self.enabled = false
 	
+	ContextActionService:UnbindAction("CHANGE_CURRENT_CAMERA")
 	RunService:UnbindFromRenderStep("UPDATE_CAMERA")
+end
+
+function Camera.destroy(self: Camera): ()
+	if self.enabled then
+		self:disable()
+	end
+	
+	for k, v in pairs(self) do
+		self[k] = nil
+	end
 end
 
 return Camera
