e5b4aa0128b0406fe3dc0a80e7efccb1190293c6
son-1u <daniel.syhatheb@yahoo.com>
2025-09-29 21:17:05 -0600

    Update input.lua

--- Vehicle/Scripts/input.lua
+++ Vehicle/Scripts/input.lua
@@ -11,6 +11,8 @@ local GuiService = game:GetService("GuiService")
 
 --------------------------------IMPORTS-------------------------------
 
+local Signal = require(script.Parent.Parent.utils.Signal)
+
 local config = require(script.Parent.Parent.utils.config).keybinds -- In the future, if we want to allow custom keybinds, move this down to the input class or whatever
 
 ------------------------------INPUT CLASS-----------------------------
@@ -24,9 +26,17 @@ export type Input = typeof(setmetatable({} :: {
 	_steer: number,
 	_current_input_type: Enum.UserInputType | nil,
 	
+	throttle_changed: typeof(Signal),
+	steering_changed: typeof(Signal),
+	gear_shift_triggered: typeof(Signal),
+	camera_change_triggered: typeof(Signal),
+	camera_rearview_triggered: typeof(Signal),
+	
 	new: () -> Input,
 	enable: (self: Input) -> (),
 	disable: (self: Input) -> (),
+	connect_mobile_inputs: (self: Input) -> (),
+	get_input_type: (self: Input) -> Enum.UserInputType,
 	get_movement_vector: (self: Input) -> (number, number),
 	destroy: (self: Input) -> (),
 }, Input))
@@ -45,7 +55,7 @@ local THROTTLE_MAP = {
 	[config.gamepad_throttle] = 1,
 	[config.kb_brake] = -1,
 	[config.kb_brake_alt] = -1,
-	[config.gamepad_brake] = 1,
+	[config.gamepad_brake] = -1,
 }
 
 local STEER_MAP = {
@@ -53,7 +63,8 @@ local STEER_MAP = {
 	[config.kb_steer_right_alt] = 1,
 	[config.kb_steer_left] = -1,
 	[config.kb_steer_left_alt] = -1,
-	[config.gamepad_steer] = 1, -- TODO: HEY!!!! I DONT KNOW FIGURE OUT HOW TO READ CONTROLLER INPUT
+	[config.gamepad_button_steer_right] = 1,
+	[config.gamepad_button_steer_left] = -1
 }
 
 local SHIFT_MAP = {
@@ -69,14 +80,35 @@ function Input.new(): Input
 		_throttle = 0,
 		_steer = 0,
 		_current_input_type = nil,
+		
+		throttle_changed = Signal.new(),
+		steering_changed = Signal.new(),
+		gear_shift_triggered = Signal.new(),
+		camera_change_triggered = Signal.new(),
+		camera_rearview_triggered = Signal.new()
 	}, Input)
 end
 
+local STEERING_DEADZONE = 0.05
+local function measure_analog_movement(input: InputObject): number
+	local position = input.Position.X
+	if math.abs(position) <= STEERING_DEADZONE then
+		return
+	end
+	
+	return (math.abs(position) - STEERING_DEADZONE) / (1 - STEERING_DEADZONE) * math.sign(position)
+end
+
+local input_type_connection
 function Input.enable(self: Input): ()
 	if self.enabled then
 		return
 	end
 	self.enabled = true
+	
+	input_type_connection = UserInputService.LastInputTypeChanged:Connect(function(input_type: Enum.UserInputType)
+		self._current_input_type = input_type
+	end)
 	
 	ContextActionService:BindAction(ACTION_NAMES.THROTTLE, function(_, input_state, input_object: InputObject)
 		local value = THROTTLE_MAP[input_object.KeyCode]
@@ -88,7 +120,8 @@ function Input.enable(self: Input): ()
 			self._throttle = value
 		else
 			self._throttle = 0
-		end 
+		end
+		self.throttle_changed:Fire(self._throttle)
 	end, false,
 		config.kb_throttle,
 		config.kb_throttle_alt,
@@ -99,7 +132,18 @@ function Input.enable(self: Input): ()
 	)
 	
 	ContextActionService:BindAction(ACTION_NAMES.STEER, function(_, input_state, input_object: InputObject)
-		local value = THROTTLE_MAP[input_object.KeyCode]
+		if input_object.KeyCode == config.mouse_steer or input_object.KeyCode == config.gamepad_steer then
+			if input_state ~= Enum.UserInputState.Change then
+				return
+			end
+
+			self._steer = measure_analog_movement(input_object)
+			self.steering_changed:Fire(self._steer)
+
+			return
+		end
+		
+		local value = STEER_MAP[input_object.KeyCode]
 		if value then
 			return
 		end
@@ -109,12 +153,15 @@ function Input.enable(self: Input): ()
 		else
 			self._steer = 0
 		end
+		self.steering_changed:Fire(self._steer)
 	end, false,
 		config.kb_steer_left,
 		config.kb_steer_left_alt,
 		config.kb_steer_right,
 		config.kb_steer_right_alt,
-		config.gamepad_steer
+		config.gamepad_steer,
+		config.gamepad_button_steer_left,
+		config.gamepad_button_steer_right
 	)
 	
 	ContextActionService:BindAction(ACTION_NAMES.SHIFT, function(_, input_state, input_object: InputObject)
@@ -123,7 +170,7 @@ function Input.enable(self: Input): ()
 			return
 		end
 		
-		-- TODO: SEND SIGNAL
+		self.gear_shift_triggered:Fire(value)
 	end, false,
 		config.kb_shift_up,
 		config.kb_shift_down,
@@ -134,14 +181,14 @@ function Input.enable(self: Input): ()
 	ContextActionService:BindAction(ACTION_NAMES.CAMERA_CHANGE, function(_, input_state, input_object: InputObject)
 		if input_object.KeyCode == config.kb_camera_rearview or input_object.KeyCode == config.gamepad_camera_rearview then
 			if input_state == Enum.UserInputState.Begin then
-				--ACTION_NAMES
+				self.camera_rearview_triggered:Fire(true)
 			else
-				--ACTION_NAMES
+				self.camera_rearview_triggered:Fire(false)
 			end
 		end
 		
 		if input_object.KeyCode == config.kb_camera_change or input_object.KeyCode == config.gamepad_camera_change then
-			-- TODO: SEND SIGNAL
+			self.camera_change_triggered:Fire()
 		end
 	end, false,
 		config.kb_camera_change,
@@ -158,9 +205,29 @@ function Input.disable(self: Input): ()
 	end
 	self.enabled = false
 	
+	if input_type_connection then
+		input_type_connection:Disconnect()
+	end
 	ContextActionService:UnbindAllActions()
 	
 	GuiService.TouchControlsEnabled = true
+end
+
+function Input.connect_mobile_inputs(self: Input, mobile_ui_object): ()
+	mobile_ui_object.throttle_changed:Connect(function(value: number)
+		self._throttle = value
+		self.throttle_changed:Fire(self._throttle)
+	end)
+	mobile_ui_object.steering_changed:Connect(function(value: number)
+		self._steer = value
+		self.steering_changed:Fire(self._steer)
+	end)
+	mobile_ui_object.gear_shift_triggered:Connect(function(value: number)
+		self.gear_shift_triggered:Fire(value)
+	end)
+	mobile_ui_object.camera_change_triggered:Connect(function()
+		self.camera_change_triggered:Fire()
+	end)
 end
 
 function Input.get_movement_vector(self: Input): (number, number)
@@ -173,6 +240,9 @@ function Input.destroy(self: Input): ()
 	end
 	
 	for k, v in pairs(self) do
+		if typeof(v) == "table" and getmetatable(v) == Signal then
+			v:DisconnectAll()
+		end
 		self[k] = nil
 	end
 end
